882438d4d316d3806e848d14e6033827c1e33f22
JAYSIMPSONS <JOAO.VIEIRA21@ESCOLA.PR.GOV.BR>
2025-02-25 07:19:47 -0300

    JOGO

---
+++ jogo de adivinhacao.py
@@ -0,0 +1,15 @@
+jogo de adivinhacao.py
+
+print("********************")
+print("bem vindo ao jogo de adivinhação")
+print("*********************")
+
+numero_secret = 2008
+
+chute_str = input("digite seu números:")
+print("Você digitou",chute_str)
+chute = int(chute_str)
+else :
+    
+print("voce errou")
+print("fim de jogo")
